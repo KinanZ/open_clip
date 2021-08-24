f155202f24bdc17a09634dbc7197ec37cf18fb15
KinanZ <kinan.z101@gmail.com>
2021-08-24 17:46:54 +0200

    first job sub

---
+++ job_bash_clip.sh
@@ -0,0 +1,32 @@
+#PBS -N clip_test_0
+#PBS -S /bin/bash
+#PBS -l nodes=1:ppn=8:gpus=2:nvidiaMin12GB,mem=16gb,walltime=24:00:00
+#PBS -j oe
+#PBS -q student
+#PBS -o /misc/student/alzouabk/Thesis/self_supervised_pretraining/moco/moco_curves/
+
+
+homePath='/misc/student/alzouabk/miniconda3'
+source $homePath/bin/activate open_clip
+
+echo "pid, gpu_utilization [%], mem_utilization [%], max_memory_usage [MiB], time [ms]"
+nvidia-smi --query-accounted-apps="pid,gpu_util,mem_util,max_memory_usage,time" --format=csv | tail -n1
+
+echo 'Training Should start'
+python3 /misc/student/alzouabk/Thesis/self_supervised_pretraining/open_clip/src/training/main.py \
+  --save-frequency 1 \
+  --zeroshot-frequency 1 \
+  --report-to tensorboard \
+  --train-data="/misc/student/alzouabk/Thesis/self_supervised_pretraining/open_clip/train_data.csv"  \
+  --val-data="/misc/student/alzouabk/Thesis/self_supervised_pretraining/open_clip/val_data.csv"  \
+  --csv-img-key filepath \
+  --csv-caption-key sentence \
+  --warmup 10000 \
+  --batch-size=16 \
+  --lr=1e-3 \
+  --wd=0.1 \
+  --epochs=3 \
+  --workers=8 \
+  --model RN50 \
+  --dist-url 'tcp://localhost:10001'
+
