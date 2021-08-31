ec190bbfab0016c9b25d3a4b539a01a3b0cccf5d
KinanZ <kinan.z101@gmail.com>
2021-08-31 18:03:02 +0200

    small dataset experiment

--- job_bash_clip_RN18.sh
+++ job_bash_clip_RN18.sh
@@ -1,8 +1,8 @@
-#PBS -N clip_run_4
+#PBS -N clip_small_dataset_RN18
 #PBS -S /bin/bash
-#PBS -l nodes=1:ppn=4:gpus=2:nvidiaMin12GB,mem=16gb,walltime=24:00:00
+#PBS -l nodes=1:ppn=4:gpus=2:nvidiaMin24GB,mem=16gb,walltime=24:00:00
 #PBS -j oe
-#PBS -q student
+#PBS -q default-cpu
 #PBS -o /misc/student/alzouabk/Thesis/self_supervised_pretraining/open_clip/outputs/
 
 
@@ -14,22 +14,22 @@ nvidia-smi --query-accounted-apps="pid,gpu_util,mem_util,max_memory_usage,time" 
 
 echo 'Training Should start'
 python3 /misc/student/alzouabk/Thesis/self_supervised_pretraining/open_clip/src/training/main.py \
-  --name='clip_run_4' \
-  --save-frequency 10 \
+  --name='clip_small_dataset_RN18' \
+  --save-frequency 20 \
   --zeroshot-frequency 1 \
   --report-to tensorboard \
-  --train-data="/misc/student/alzouabk/Thesis/self_supervised_pretraining/open_clip/train_data.csv"  \
-  --val-data="/misc/student/alzouabk/Thesis/self_supervised_pretraining/open_clip/val_data.csv"  \
+  --train-data="/misc/student/alzouabk/Thesis/self_supervised_pretraining/open_clip/train_data_no_0.csv"  \
+  --val-data="/misc/student/alzouabk/Thesis/self_supervised_pretraining/open_clip/val_data_no_0.csv"  \
   --csv-img-key filepath \
   --csv-caption-key sentence \
   --csv-separator="," \
   --warmup 2500 \
-  --batch-size=28 \
+  --batch-size=48 \
   --lr=0.0002 \
   --wd=0.1 \
-  --epochs=100 \
+  --epochs=2 \
   --workers=8 \
-  --model RN50 \
+  --model RN18 \
   --default-aug \
   --dist-url 'tcp://localhost:10001'
 
--- src/clip/clip.py
+++ src/clip/clip.py
@@ -219,7 +219,7 @@ def load(name: str, device: Union[str, torch.device] = "cuda" if torch.cuda.is_a
            transform_val
 
 
-def tokenize(texts: Union[str, List[str]], context_length: int = 77) -> torch.LongTensor:
+def tokenize(texts: Union[str, List[str]], context_length: int = 128) -> torch.LongTensor:
     """
     Returns the tokenized representation of given input string(s)
     Parameters
