b32854f403dc08987408a00d62066b46ca6234fd
KinanZ <kinan.z101@gmail.com>
2021-10-04 17:07:10 +0200

    finally got it

--- job_bash_clip_RN18.sh
+++ job_bash_clip_RN18.sh
@@ -1,6 +1,6 @@
 #PBS -N clip_RN18_custom_loss_3_old_eval
 #PBS -S /bin/bash
-#PBS -l nodes=1:ppn=4:gpus=4:nvidiaMin12GB,mem=16gb,walltime=24:00:00
+#PBS -l nodes=1:ppn=4:gpus=4:ubuntu2004:nvidiaMin12GB,mem=16gb,walltime=24:00:00
 #PBS -j oe
 #PBS -o /misc/student/alzouabk/Thesis/self_supervised_pretraining/open_clip/outputs/
 
