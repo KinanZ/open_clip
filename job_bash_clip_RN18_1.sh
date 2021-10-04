0dfbeaeeaa748eed7af86bdef788c8d24ae7a31b
KinanZ <kinan.z101@gmail.com>
2021-10-04 18:47:03 +0200

    finally got it

--- job_bash_clip_RN18_1.sh
+++ job_bash_clip_RN18_1.sh
@@ -23,7 +23,7 @@ python3 /misc/student/alzouabk/Thesis/self_supervised_pretraining/open_clip/src/
   --csv-label-key labels \
   --csv-separator="," \
   --warmup 2500 \
-  --batch-size=36 \
+  --batch-size=35 \
   --lr=0.0002 \
   --wd=0.1 \
   --epochs=4 \
