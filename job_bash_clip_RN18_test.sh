5584b82689996ef13fd7364cae27fa6cfc294b2f
KinanZ <kinan.z101@gmail.com>
2021-10-06 11:20:20 +0200

    again 3090 test

--- job_bash_clip_RN18_test.sh
+++ job_bash_clip_RN18_test.sh
@@ -1,6 +1,6 @@
-#PBS -N clip_RN18_3090_test
+#PBS -N clip_RN18_p100_test
 #PBS -S /bin/bash
-#PBS -l nodes=1:ppn=4:gpus=1:ubuntu2004:nvidiaRTX3090,mem=16gb,walltime=24:00:00
+#PBS -l nodes=1:ppn=4:gpus=1:ubuntu2004:nvidiaP100,mem=16gb,walltime=24:00:00
 #PBS -j oe
 #PBS -o /misc/student/alzouabk/Thesis/self_supervised_pretraining/open_clip/outputs/
 
@@ -13,7 +13,7 @@ nvidia-smi --query-accounted-apps="pid,gpu_util,mem_util,max_memory_usage,time" 
 
 echo 'Training Should start'
 python3 /misc/student/alzouabk/Thesis/self_supervised_pretraining/open_clip/src/training/main.py \
-  --name='clip_RN18_3090_test' \
+  --name='clip_RN18_p100_test' \
   --save-frequency 199 \
   --report-to tensorboard \
   --train-data="/misc/student/alzouabk/Thesis/self_supervised_pretraining/open_clip/train_data_no_dup_w_labels.csv"  \
