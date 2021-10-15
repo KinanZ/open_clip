6d052362a5ecb5eaf695ba1c43315b5ef8bb88e6
KinanZ <kinan.z101@gmail.com>
2021-10-15 15:03:01 +0200

    first run on 3090

--- job_bash_clip_RN18_11.sh
+++ job_bash_clip_RN18_11.sh
@@ -1,6 +1,6 @@
 #PBS -N clip_RN18_custom_loss_3_3090
 #PBS -S /bin/bash
-#PBS -l nodes=1:ppn=8:gpus=4:ubuntu2004:nvidiaRTX3090,mem=16gb,walltime=24:00:00
+#PBS -l nodes=1:ppn=4:gpus=2:ubuntu2004:nvidiaRTX3090,mem=16gb,walltime=24:00:00
 #PBS -j oe
 #PBS -o /misc/student/alzouabk/Thesis/self_supervised_pretraining/open_clip/outputs/
 
@@ -26,7 +26,7 @@ python3 /misc/student/alzouabk/Thesis/self_supervised_pretraining/open_clip/src/
   --batch-size=64 \
   --lr=0.0002 \
   --wd=0.1 \
-  --epochs=300 \
+  --epochs=3 \
   --workers=8 \
   --model RN18 \
   --custom-loss-3 \
