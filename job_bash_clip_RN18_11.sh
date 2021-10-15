d6027eb7cf687fa5b0e094eeb5742a9553da82e5
KinanZ <kinan.z101@gmail.com>
2021-10-15 17:29:22 +0200

    first run on 3090

--- job_bash_clip_RN18_11.sh
+++ job_bash_clip_RN18_11.sh
@@ -1,4 +1,4 @@
-#PBS -N clip_RN18_default_loss_3090
+#PBS -N clip_RN18_custom_loss_3_3090
 #PBS -S /bin/bash
 #PBS -l nodes=1:ppn=4:gpus=4:ubuntu2004:nvidiaRTX3090,mem=64gb,walltime=24:00:00
 #PBS -j oe
@@ -13,7 +13,7 @@ nvidia-smi --query-accounted-apps="pid,gpu_util,mem_util,max_memory_usage,time" 
 
 echo 'Training Should start'
 python3 /misc/student/alzouabk/Thesis/self_supervised_pretraining/open_clip/src/training/main.py \
-  --name='clip_RN18_default_loss_3090' \
+  --name='clip_RN18_custom_loss_3_3090' \
   --save-frequency 199 \
   --report-to tensorboard \
   --train-data="/misc/student/alzouabk/Thesis/self_supervised_pretraining/open_clip/train_data_no_dup_w_labels.csv"  \
