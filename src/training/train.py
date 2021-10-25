18bddffbf1c9b11f45f4b55b80afa0f324895d50
KinanZ <kinan.z101@gmail.com>
2021-10-25 16:52:34 +0200

    t sne with pca

--- src/training/train.py
+++ src/training/train.py
@@ -320,17 +320,18 @@ def evaluate(model, data, epoch, args, tb_writer=None, steps=None):
                 for name, val in metrics.items():
                     tb_writer.add_scalar(f"val/{name}", val, epoch)
                 if args.t_sne:
-                    all_labels = torch.cat(all_labels)
-                    for index in range(all_labels.shape[0]):
-                        all_labels[index] = onehot_to_int(all_labels[index])
+                    all_labels_onehot = torch.cat(all_labels)
+                    all_labels_int = []
+                    for index in range(all_labels_onehot.shape[0]):
+                        all_labels_int.append(onehot_to_int(all_labels_onehot[index]))
                     pca = decomposition.PCA(n_components=36)
                     pca.fit(torch.cat(all_image_features))
                     all_image_features = pca.transform(torch.cat(all_image_features))
                     pca.fit(torch.cat(all_text_features))
                     all_text_features = pca.transform(torch.cat(all_text_features))
-                    tb_writer.add_embedding(mat=all_image_features, metadata=all_labels,
+                    tb_writer.add_embedding(mat=all_image_features, metadata=all_labels_int,
                                             global_step=epoch, tag='val_image_features')
-                    tb_writer.add_embedding(mat=all_text_features, metadata=all_labels,
+                    tb_writer.add_embedding(mat=all_text_features, metadata=all_labels_int,
                                             global_step=epoch, tag='val_text_features')
         if args.wandb:
             for name, val in metrics.items():
@@ -452,18 +453,19 @@ def evaluate_train(model, data, epoch, args, tb_writer=None, steps=None):
                 for name, val in metrics.items():
                     tb_writer.add_scalar(f"train_eval/{name}", val, epoch)
                 if args.t_sne:
-                    all_labels = torch.cat(all_labels)
-                    for index in range(all_labels.shape[0]):
-                        all_labels[index] = onehot_to_int(all_labels[index])
+                    all_labels_onehot = torch.cat(all_labels)
+                    all_labels_int = []
+                    for index in range(all_labels_onehot.shape[0]):
+                        all_labels_int.append(onehot_to_int(all_labels_onehot[index]))
                     pca = decomposition.PCA(n_components=36)
                     pca.fit(torch.cat(all_image_features))
                     all_image_features = pca.transform(torch.cat(all_image_features))
                     pca.fit(torch.cat(all_text_features))
                     all_text_features = pca.transform(torch.cat(all_text_features))
-                    tb_writer.add_embedding(mat=all_image_features, metadata=all_labels,
-                                            global_step=epoch, tag='val_image_features')
-                    tb_writer.add_embedding(mat=all_text_features, metadata=all_labels,
-                                            global_step=epoch, tag='val_text_features')
+                    tb_writer.add_embedding(mat=all_image_features, metadata=all_labels_int,
+                                            global_step=epoch, tag='train_image_features')
+                    tb_writer.add_embedding(mat=all_text_features, metadata=all_labels_int,
+                                            global_step=epoch, tag='train_text_features')
 
         if args.wandb:
             for name, val in metrics.items():
