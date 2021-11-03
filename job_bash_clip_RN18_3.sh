d7c71af1bf03176040b17b8920e468fe16f1ac04
KinanZ <kinan.z101@gmail.com>
2021-11-03 17:11:51 +0100

    de model

--- job_bash_clip_RN18_3.sh
+++ job_bash_clip_RN18_3.sh
@@ -1,4 +1,4 @@
-#PBS -N clip_RN18_de_tokenize_de_model
+#PBS -N clip_RN18_de_tokenize_de_model_eh
 #PBS -S /bin/bash
 #PBS -l nodes=1:ppn=6:gpus=2:ubuntu2004:nvidiaTITANX,mem=16gb,walltime=24:00:00
 #PBS -j oe
@@ -13,7 +13,7 @@ nvidia-smi --query-accounted-apps="pid,gpu_util,mem_util,max_memory_usage,time" 
 
 echo 'Training Should start'
 python3 /misc/student/alzouabk/Thesis/self_supervised_pretraining/open_clip/src/training/main.py \
-  --name='clip_RN18_de_tokenize_de_model' \
+  --name='clip_RN18_de_tokenize_de_model_eh' \
   --save-frequency 199 \
   --report-to tensorboard \
   --t-sne \
