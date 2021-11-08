490a1bf40f74ce9b703c7a2135a3721d39615660
KinanZ <kinan.z101@gmail.com>
2021-11-08 21:13:26 +0100

    aug text for real

--- job_bash_clip_RN18.sh
+++ job_bash_clip_RN18.sh
@@ -1,8 +1,8 @@
-#PBS -N clip_RN18_text_aug_skip_some
+#PBS -N clip_RN18_default_closs3_again
 #PBS -S /bin/bash
 #PBS -l nodes=1:ppn=6:gpus=4:ubuntu2004:nvidiaTITANX,mem=16gb,walltime=24:00:00
 #PBS -j oe
-#PBS -o /misc/student/alzouabk/Thesis/self_supervised_pretraining/open_clip/outputs/
+#PBS -o /misc/student/alzouabk/Thesis/self_supervised_pretraining/open_clip/outputs_final/
 
 
 homePath='/misc/student/alzouabk/miniconda3'
@@ -13,11 +13,11 @@ nvidia-smi --query-accounted-apps="pid,gpu_util,mem_util,max_memory_usage,time" 
 
 echo 'Training Should start'
 python3 /misc/student/alzouabk/Thesis/self_supervised_pretraining/open_clip/src/training/main.py \
-  --name='clip_RN18_text_aug_skip_some' \
-  --save-frequency 199 \
+  --name='clip_RN18_default_closs3_again' \
+  --save-frequency 49 \
   --report-to tensorboard \
   --t-sne \
-  --logs='/misc/student/alzouabk/Thesis/self_supervised_pretraining/open_clip/outputs/' \
+  --logs='/misc/student/alzouabk/Thesis/self_supervised_pretraining/open_clip/outputs_final/' \
   --train-data="/misc/student/alzouabk/Thesis/self_supervised_pretraining/open_clip/train_data_no_dup_w_labels_bboxes.csv"  \
   --val-data="/misc/student/alzouabk/Thesis/self_supervised_pretraining/open_clip/val_data_no_dup_w_labels_bboxes.csv"  \
   --csv-img-key filepath \
@@ -26,16 +26,15 @@ python3 /misc/student/alzouabk/Thesis/self_supervised_pretraining/open_clip/src/
   --csv-bbox-key bboxes \
   --csv-separator="," \
   --warmup 2500 \
-  --batch-size=35 \
+  --batch-size=36 \
   --lr=0.00015 \
   --wd=0.1 \
-  --epochs=200 \
+  --epochs=100 \
   --workers=4 \
-  --model RN18 \
   --custom-loss-3 \
   --default-aug-img \
   --eval-train \
-  --skip-aug-text \
   --custom-eval \
   --seed=101 \
-  --dist-url 'tcp://localhost:10026'
+  --model RN18 \
+  --dist-url 'tcp://localhost:10025'
