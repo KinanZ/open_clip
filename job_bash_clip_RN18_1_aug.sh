5c7589d4205141b10cd319f603152218d8df2c99
KinanZ <kinan.z101@gmail.com>
2021-11-24 12:16:56 +0100

    Legit now

--- job_bash_clip_RN18_1_aug.sh
+++ job_bash_clip_RN18_1_aug.sh
@@ -1,4 +1,4 @@
-#PBS -N Healthy_Caption_grouped_set_aug_12
+#PBS -N Healthy_Caption_grouped_set_aug_223
 #PBS -S /bin/bash
 #PBS -l nodes=1:ppn=6:gpus=4:ubuntu2004:nvidiaGTX1080Ti,mem=36gb,walltime=24:00:00
 #PBS -j oe
@@ -13,7 +13,7 @@ nvidia-smi --query-accounted-apps="pid,gpu_util,mem_util,max_memory_usage,time" 
 
 echo 'Training Should start'
 python3 /misc/student/alzouabk/Thesis/self_supervised_pretraining/open_clip/src/training/main.py \
-  --name='Healthy_Caption_grouped_set_aug_12' \
+  --name='Healthy_Caption_grouped_set_aug_223' \
   --save-frequency 100 \
   --report-to tensorboard \
   --t-sne \
@@ -36,7 +36,7 @@ python3 /misc/student/alzouabk/Thesis/self_supervised_pretraining/open_clip/src/
   --eval-train \
   --use-de-tokenizer \
   --custom-eval \
-  --seed=12 \
+  --seed=223 \
   --model RN18 \
   --new-model \
   --embid-dim=512 \
