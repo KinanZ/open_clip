c1f6f847579eae1765f17502a174b2200c1c5672
KinanZ <kinan.z101@gmail.com>
2021-11-29 19:59:19 +0100

    Legit now

--- job_bash_clip_RN18_4.sh
+++ job_bash_clip_RN18_4.sh
@@ -1,4 +1,4 @@
-#PBS -N Healthy_Caption_grouped_default_333
+#PBS -N Healthy_Caption_grouped_default_l_333
 #PBS -S /bin/bash
 #PBS -l nodes=1:ppn=6:gpus=4:ubuntu2004:nvidiaGTX1080Ti,mem=36gb,walltime=24:00:00
 #PBS -j oe
@@ -13,7 +13,7 @@ nvidia-smi --query-accounted-apps="pid,gpu_util,mem_util,max_memory_usage,time" 
 
 echo 'Training Should start'
 python3 /misc/student/alzouabk/Thesis/self_supervised_pretraining/open_clip/src/training/main.py \
-  --name='Healthy_Caption_grouped_default_333' \
+  --name='Healthy_Caption_grouped_default_l_333' \
   --save-frequency 150 \
   --report-to tensorboard \
   --t-sne \
